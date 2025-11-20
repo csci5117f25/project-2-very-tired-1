521adf58cbba9ba2d7f07e4a5c743883fd1ddac8
Alex Knusel <apknusel@yahoo.com>
2025-11-19 23:19:08 -0600

    changes

--- pwa-assets.config.js
+++ pwa-assets.config.js
@@ -1,6 +1,3 @@
 import { defineConfig, minimal2023Preset as preset } from '@vite-pwa/assets-generator/config'
 
-export default defineConfig({
-  preset,
-  images: ['public/logo.png'],
-})
+export default defineConfig({ preset, images: ['public/logo.png'] })
