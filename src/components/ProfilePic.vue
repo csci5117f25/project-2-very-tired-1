ebc5c2c36a66d7d6f71cb67d5e708d997102784f
Shuo Jiang <shuovok@gmail.com>
2025-11-24 18:44:49 -0600

    added basecard component and completed the main view

---
+++ src/components/ProfilePic.vue
@@ -0,0 +1,26 @@
+<script setup>
+// --- Props ---
+const props = defineProps({
+  src: { type: String, default: null },
+  alt: { type: String, default: 'Profile Picture' },
+  size: { type: Number, default: 150 },
+})
+
+const srcToUse = props.src || '/pwa-512x512.png'
+</script>
+
+<template>
+  <div class="avatar-wrapper" :style="{ width: size + 'px', height: size + 'px' }">
+    <img :src="srcToUse" :alt="alt" class="avatar-img" />
+  </div>
+</template>
+
+<style scoped>
+.avatar-wrapper {
+  border-radius: 50%;
+  overflow: hidden;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { useRouter } from 'vue-router'
-
+import BaseCard from '@/components/BaseCard.vue'
+import ProfilePic from '@/components/ProfilePic.vue'
 const router = useRouter()
 
 function goStartHike() {
@@ -9,9 +10,50 @@ function goStartHike() {
 </script>
 
 <template>
-  <div id="app">
-    <b-button @click="goStartHike">Start Hike</b-button>
+  <div class="rows">
+    <div class="row">
+      <div class="profile-column">
+        <profile-pic :size="150" />
+      </div>
+      <base-card link="" size="half">Place Holder for weather</base-card>
+    </div>
+
+    <div class="row">
+      <base-card link="/previousHikes" size="full">Previous Hikes</base-card>
+    </div>
+
+    <div class="row">
+      <base-card link="" size="full">Calendar</base-card>
+    </div>
+
+    <div class="row">
+      <base-card link="/startHike" size="half">Start hike</base-card>
+      <base-card link="" size="half">Goals</base-card>
+    </div>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.rows {
+  display: flex;
+  flex-direction: column;
+  gap: 25px;
+  height: 100vh;
+  padding: 5vh 3vw 10vh 3vw;
+}
+
+.row {
+  flex: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
+}
+
+.profile-column {
+  width: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
